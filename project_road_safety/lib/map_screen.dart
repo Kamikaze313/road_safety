1ee70c123de2e267a0a941aa67c84ab77b903f5a
Najid <najidniju2@gmail.com>
2024-04-01 20:59:38 +0530

    herecomesgov

--- project_road_safety/lib/map_screen.dart
+++ project_road_safety/lib/map_screen.dart
@@ -75,29 +75,29 @@ class _MapScreenState extends State<MapScreen> {
     // Make the second API request for road conditions
     try {
       var roadConditionsResponse = await http.get(
-  Uri.parse('http://172.232.106.170:3000/roadConditions?latitude=${widget.startCord.latitude}&longitude=${widget.startCord.longitude}'),
-);
-
+        Uri.parse(
+            'http://172.232.106.170:3000/roadConditions?latitude=${widget.startCord.latitude}&longitude=${widget.startCord.longitude}'),
+      );
 
       // Process the second API response
       if (roadConditionsResponse.statusCode == 200) {
         var roadConditionsData = jsonDecode(roadConditionsResponse.body);
-        
+
         // Initialize listofroadconditions
         List<dynamic> listofroadconditions = [];
-        
+
         // Assuming road conditions data is in the same format as the provided example
         for (var feature in roadConditionsData) {
           var coordinates = feature['geometry']['coordinates'];
           var type = feature['properties']['type'];
-          
+
           // Add road conditions data to listofroadconditions
           listofroadconditions.add({
             'coordinates': coordinates,
             'type': type,
           });
         }
-        
+
         // Update the UI
         setState(() {
           // Assign listofroadconditions to the state variable
@@ -105,104 +105,100 @@ class _MapScreenState extends State<MapScreen> {
         });
       } else {
         // Handle the error or unexpected response
-        print('Failed to fetch road conditions. Status code: ${roadConditionsResponse.statusCode}');
+        print(
+            'Failed to fetch road conditions. Status code: ${roadConditionsResponse.statusCode}');
       }
     } catch (error) {
       // Handle any exceptions that occurred during the request
       print('Error fetching road conditions: $error');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ColorFiltered(
-        colorFilter: ColorFilter.mode(
-          Color.fromARGB(255, 158, 8, 207).withOpacity(1.0), // Keep hue and brightness constant
-          BlendMode.saturation,
-        ),
-        child: FlutterMap(
-          options: MapOptions(zoom: 14, center: currentLocation),
-          children: [
-            TileLayer(
-              urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-              userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-            ),
-            PolylineLayer(
-              polylineCulling: false,
-              polylines: [
-                Polyline(
-                  points: points,
-                  color: Color.fromARGB(255, 44, 44, 44),
-                  strokeWidth: 7,
+      body: FlutterMap(
+        options: MapOptions(zoom: 14, center: currentLocation),
+        children: [
+          TileLayer(
+            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+            userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+          ),
+          PolylineLayer(
+            polylineCulling: false,
+            polylines: [
+              Polyline(
+                points: points,
+                color: Color.fromARGB(255, 44, 44, 44),
+                strokeWidth: 7,
+              ),
+              Polyline(
+                points: points,
+                color: Color.fromARGB(255, 189, 60, 60),
+                strokeWidth: 2,
+              ),
+            ],
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: widget.endCord,
+                width: 80,
+                height: 80,
+                child: IconButton(
+                  onPressed: () {
+                    print('End Marker tapped!');
+                  },
+                  icon: const Icon(Icons.location_on),
+                  color: Colors.red,
+                  iconSize: 45,
                 ),
-                Polyline(
-                  points: points,
-                  color: Color.fromARGB(255, 189, 60, 60),
-                  strokeWidth: 2,
+              ),
+              Marker(
+                point: currentLocation, // Use currentLocation as the point
+                width: 80,
+                height: 80,
+                child: Icon(
+                  Icons.directions_car_filled_rounded,
+                  color: Color.fromARGB(255, 26, 57, 83),
+                  size: 45,
                 ),
-              ],
-            ),
-            MarkerLayer(
-              markers: [
+              ),
+              // Add markers for each road condition
+              for (var feature in listOfRoadConditions)
                 Marker(
-                  point: widget.endCord,
+                  point: LatLng(
+                    feature['coordinates'][1].toDouble(),
+                    feature['coordinates'][0].toDouble(),
+                  ),
                   width: 80,
                   height: 80,
-                  child: IconButton(
-                    onPressed: () {
-                      print('End Marker tapped!');
-                    },
-                    icon: const Icon(Icons.location_on),
-                    color: Colors.red,
-                    iconSize: 45,
-                  ),
+                  child: _buildMarkerIcon(feature['type']),
                 ),
-                Marker(
-                  point: currentLocation, // Use currentLocation as the point
-                  width: 80,
-                  height: 80,
-                  child: Icon(
-                    Icons.directions_car_filled_rounded,
-                    color: Color.fromARGB(255, 26, 57, 83),
-                    size: 45,
-                  ),
-                ),
-                // Add markers for each road condition
-                for (var feature in listOfRoadConditions)
-                  Marker(
-                    point: LatLng(
-                      feature['coordinates'][1].toDouble(),
-                      feature['coordinates'][0].toDouble(),
-                    ),
-                    width: 80,
-                    height: 80,
-                    child: _buildMarkerIcon(feature['type']),
-                  ),
-              ],
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
   // Helper function to build marker icon based on the type
-Widget _buildMarkerIcon(String type) {
-  // Determine marker icon based on the type
-  String assetName;
-  if (type == 'pothole') {
-    assetName = 'assets/pothole.png';
-  } else if (type == 'speed_breaker') {
-    assetName = 'assets/speed_breaker.png';
-  } else {
-    // Default icon
-    assetName = 'assets/default_icon.png'; // Path to a default icon
-  }
+  Widget _buildMarkerIcon(String type) {
+    // Determine marker icon based on the type
+    String assetName;
+    if (type == 'pothole') {
+      assetName = 'assets/pothole.png';
+    } else if (type == 'speed_breaker') {
+      assetName = 'assets/speed_breaker.png';
+    } else {
+      // Default icon
+      assetName = 'assets/default_icon.png'; // Path to a default icon
+    }
 
-  return Image.asset(
-    assetName,
-    width: 45,
-    height: 45,
-  );
-}
+    return Image.asset(
+      assetName,
+      width: 45,
+      height: 45,
+    );
+  }
 }
